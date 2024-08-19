3b04afd23e957581f36780cdaa6fc99cf194c3d5
muhazzar <muhazzar639@gmail.com>
2024-08-19 15:07:55 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# yo
+
+print ("hello")
+
+a=9
+b=8
+print(a)
+print("a")
+print("nilai a =", a)
+print("nilai b =", b)
+
+#assigment indrct
+x=b
+
+#contoh penamaan variabel
+nilai_x=20 #menggunakan underscr
+ratus2=200 #angka tidak bisa di depan
+
+#integer
+data_int = 1
+print("data :" ,data_int ,"bertipe" ,type(data_int))
+
+#float
+datafloat = 1.9
+print("data :" ,datafloat ,"bertipe" ,type(datafloat))
+
+#string
+data_string = "yo"
+print("data :" ,data_string ,"bertipe" ,type(data_string))
+
+#boolean
+data_bool = True
+print("data :" ,data_bool ,"bertipe" ,type(data_bool))
