b033db2dec9b2fe0feeb8387b427545f34a12c83
muhazzar <muhazzar639@gmail.com>
2024-08-19 16:55:15 +0700

    menambahkan file casting

---
+++ casting_tipe_data/main.py
@@ -0,0 +1,8 @@
+data_int=9
+data_float=float(data_int)
+data_str=str(data_int)
+data_bool=bool(data_int)
+print("data :" ,data_int) 
+print("data :" ,data_float)
+print("data :" ,data_str)
+print("data :" ,data_bool)
