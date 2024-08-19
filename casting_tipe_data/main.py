5356b8ae0add38868745b77ff4a56940fb42dc00
muhazzar <muhazzar639@gmail.com>
2024-08-19 17:32:23 +0700

    yo

--- casting_tipe_data/main.py
+++ casting_tipe_data/main.py
@@ -1,8 +1,53 @@
-data_int=9
+#casting tipe data
+
+#INTEGER
+print("INT")
+data_int  =9
+
 data_float=float(data_int)
-data_str=str(data_int)
-data_bool=bool(data_int)
-print("data :" ,data_int) 
-print("data :" ,data_float)
-print("data :" ,data_str)
-print("data :" ,data_bool)
+data_str  =str(data_int)
+data_bool =bool(data_int)
+
+print("data :" ,data_int,type(data_int)) 
+print("data :" ,data_float,type(data_float))
+print("data :" ,data_str,type(data_str))
+print("data :" ,data_bool,type(data_bool))
+
+#FLOAT
+print("FLOAT")
+data_float=9.9
+
+data_int  =int(data_float)
+data_str  =str(data_float)
+data_bool =bool(data_float)
+
+print("data :" ,data_int,type(data_int)) 
+print("data :" ,data_float,type(data_float))
+print("data :" ,data_str,type(data_str))
+print("data :" ,data_bool,type(data_bool))
+
+#STRING
+print("STRING")
+data_str="1"
+
+data_int    =int(data_str)
+data_float  =float(data_str)
+data_bool   =bool(data_str)
+
+print("data :" ,data_int,type(data_int)) 
+print("data :" ,data_float,type(data_float))
+print("data :" ,data_str,type(data_str))
+print("data :" ,data_bool,type(data_bool))
+
+#BOOLEAN
+print("BOOLEAN")
+data_bool=False
+
+data_int    =int(data_bool)
+data_float  =float(data_bool)
+data_str   =str(data_bool)
+
+print("data :" ,data_int,type(data_int)) 
+print("data :" ,data_float,type(data_float))
+print("data :" ,data_str,type(data_str))
+print("data :" ,data_bool,type(data_bool))
