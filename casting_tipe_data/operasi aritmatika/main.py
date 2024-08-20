8601dec89c1d8bcf83d4492f0725a071766645b7
muhazzar <muhazzar639@gmail.com>
2024-08-20 14:18:25 +0700

    file baru

---
+++ casting_tipe_data/operasi aritmatika/main.py
@@ -0,0 +1,38 @@
+#operasi tambah +
+a=10
+b=3
+hasil=a+b
+print(a,'+',b,'=',hasil) 
+
+#operasi kurang -
+hasil=a-b
+print(a,'-',b,'=',hasil) 
+
+#operasi kali *
+hasil=a*b
+print(a,'*',b,'=',hasil) 
+
+#operasi bagi /
+hasil=a/b
+print(a,'/',b,'=',hasil) 
+
+#operasi eksponen (pangkat)**
+hasil=a**b
+print(a,'**',b,'=',hasil) 
+
+#operasi modulus %
+hasil=a%b
+print(a,'%',b,'=',hasil) 
+
+#operasi floor division //
+
+hasil=a//b
+print(a,'//',b,'=',hasil)  
+
+#prioritas operasi (),exponen,perkalian,pertambahan
+a=3
+s=5
+d=7
+hasil=a+s*d
+print(a,'+',s,'*',d,'=',hasil)
+print((a,'+',s),'*',d,'=',hasil)
---
+++ input data/main.py
@@ -0,0 +1,11 @@
+data=input("masukan data :")
+print("data =",data,type(data))
+
+data_int=int(input("masukan angka :"))
+print("data =",data_int,type(data_int))
+
+data_float=float(input("masukan angka :"))
+print("data =",data_float,type(data_float))
+
+data_bool=bool(input("masukan data :"))
+print("data =",data_bool,type(data_bool))
