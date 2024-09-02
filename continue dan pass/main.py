86fddc0146b971bee6b62e7a5ee7396abb69c02e
muhazzar <muhazzar639@gmail.com>
2024-09-02 14:25:54 +0700

    looop

---
+++ continue dan pass/main.py
@@ -0,0 +1,10 @@
+# continue 
+angka = 0
+while angka < 5:
+    angka+=1
+    print(f"angka >{angka}")
+    if angka ==3:
+        print("this 3")
+        continue
+    print("YOI")
+
---
+++ for loop/main.py
@@ -0,0 +1,16 @@
+# list
+angka = [1,2,3,4,5,6]
+for i in angka :
+    print(f"i sekarang {i}")
+print('akhir program 1\n')
+
+# range
+angka_range = range(5)
+for i in angka_range :
+    print(f"i sekarang {i}")
+print('akhir program 2\n')
+
+# string 
+data_str = "kalapa muda"
+for i in data_str:
+    print(i)
