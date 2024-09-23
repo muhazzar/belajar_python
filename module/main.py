8e36a4febfbe39b6abf3a8a1837cfbe2fb491e5f
muhazzar <muhazzar639@gmail.com>
2024-09-23 14:35:52 +0700

    modified

--- module/main.py
+++ module/main.py
@@ -1,9 +1,10 @@
 import matika
 
 hasil_tambah=matika.tambah(5,5,5,5,5,5,5,5,5,5)
-hasil_kali=matika.kali(8,7)
-hasil_pangkat=matika.pangkat(3)
-
 print (f"pertambahan: {hasil_tambah}")
+
+hasil_kali=matika.kali(8,7)
 print(f"perkalian: {hasil_kali}")
+
+hasil_pangkat=matika.pangkat(3)
 print(f"pangkat3: {hasil_pangkat(3)}")
